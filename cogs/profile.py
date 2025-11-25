be36c2102c739038c779d273bc813bbc9bd52290
PedroCN <pedrocn236@gmail.com>
2025-11-25 16:56:30 -0400

    adicionando comando profile para testes

---
+++ cogs/profile.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+from PIL import Image, ImageDraw, ImageFont
+from io import BytesIO
+
+class Profile(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+        
+    @commands.command(name="perfil")
+    async def profile(self, ctx, member:discord.Member = None):
+        member = member or ctx.author
+        
+        img = Image.new("RGB", (600, 300), (45, 45, 45))
+        draw = ImageDraw.Draw(img)
+        
+        buffer = BytesIO()
+        img.save(buffer, format="PNG")
+        buffer.seek(0)
+        
+        await ctx.send(file=discord.File(buffer, filename="perfil.png"))
+        
+async def setup(bot):
+    await bot.add_cog(Profile(bot))
